22cbd7502b603b253be44106a017c309d0635669
abhimanyu012 <abhimanyucode2021@gmail.com>
2026-09-21 16:50:19 +0530

    refactor: separate server startup logic into new server entry point

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const server = require("./src/app.js");
+
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,4 @@ app.get("/", (req, res) => {
   console.log("Hii from the backend");
   res.send("Hello World");
 });
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+module.exports = app;
