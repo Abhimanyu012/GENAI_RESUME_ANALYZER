bc18fa4cbdff1e71c2f990dbb0138c81f91730eb
abhimanyu012 <abhimanyucode2021@gmail.com>
2026-09-21 16:36:20 +0530

    feat: initialize express server with basic routing and json middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,12 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Hii from the backend");
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
