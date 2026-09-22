1d3f3b3a20850b2ebf3ccf4143ca5d7e18b27a42
abhimanyu012 <abhimanyucode2021@gmail.com>
2026-09-22 16:40:30 +0530

    feat: integrate MongoDB database connection and add dev script

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const connectDb = require("./config/database");
 const app = express();
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   console.log("Hii from the backend");
   res.send("Hello World");
 });
+connectDb();  
 module.exports = app;
